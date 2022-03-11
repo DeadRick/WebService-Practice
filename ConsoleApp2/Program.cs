ec4d664e6e0d02da452d138774afc0868e67f634
DeadRick <vndemyanenko@edu.hse.ru>
2022-03-11 11:44:08 +0300

    - Keep going

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        class Person
+        {
+            public Person()
+            {
+                Name = "Bob";
+                Age = 18;
+            }
+            public Person(string name)
+            {
+                Name = name;
+                Age = 18;
+            }
+
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+
+            Person person2 = new Person("John");
+            Console.WriteLine(person2.Name);
+            Console.WriteLine(person2.Age);
+        }
+    }
+}
---
+++ WebApplication4/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    [Route("/api/[controller]")]
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private static List<Product> products = new List<Product>(new[] {
+        new Product() { Id = 1, Name = "Notebook", Price = 100000 },
+        new Product() { Id = 2, Name = "Car", Price = 2000000 },
+        new Product() { Id = 3, Name = "Apple", Price = 30 },
+});
+        
+        [HttpGet]
+        public IEnumerable<Product> Get() => products;
+
+
+        [HttpGet("{id}")]           // параметр для маршрутизации
+        public IActionResult Get(int id)
+        {
+            var product = products.SingleOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+    }
+}
